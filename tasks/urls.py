a1d4b1de7c7a6a90f0a69afc42460f6da1cdb446
muslima <muslimazokirjonova2004@gmail.com>
2023-09-18 16:01:27 +0500

    Initial commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,11 +5,11 @@ from tasks.views import BoardListAPIView, BoardCreateAPIView, BoardColumnListAPI
 
 urlpatterns = [
     # path('', include(router.urls)),
-    path('board-list', BoardListAPIView.as_view(), name='board-list'),
+    path('board_list', BoardListAPIView.as_view(), name='board-list'),
     path('column-list', ColumnListAPIView.as_view(), name='column-list'),
-    path('board-create', BoardCreateAPIView.as_view(), name='board-create'),
-    path('task-create', TaskCreateAPIView.as_view(), name='task-create'),
-    path('board-column-list/<int:board_id>', BoardColumnListAPIView.as_view(), name='board-column-list'),
-    path('column-task-list/<int:column_id>', TaskListByColumnAPIView.as_view(), name='column-task-list'),
+    path('board', BoardCreateAPIView.as_view(), name='board-create'),
+    path('task', TaskCreateAPIView.as_view(), name='task-create'),
+    path('board_id/<int:board_id>', BoardColumnListAPIView.as_view(), name='board-column-list'),
+    path('column_id/<int:column_id>', TaskListByColumnAPIView.as_view(), name='column-task-list'),
 
 ]
