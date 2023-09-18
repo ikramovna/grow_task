7d486bc32c44d53a309d6b574244cde5b2f0df1c
muslima <muslimazokirjonova2004@gmail.com>
2023-09-18 16:36:10 +0500

    Initial commit

---
+++ shared/models.py
@@ -0,0 +1,9 @@
+from django.db.models import DateTimeField, Model
+
+
+class BaseModel(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
